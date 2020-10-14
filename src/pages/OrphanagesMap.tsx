77d789935e634497db6228a52ae2a03b8506bcf0
Samuel Rodrigues <samuelclerod@gmail.com>
2020-10-14 18:05:59 -0300

    adicionado conexão com backend

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import { Map, TileLayer } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
-import 'leaflet/dist/leaflet.css'
-
-import mapLocalMarker from '../images/local.svg';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import '../styles/pages/OrphanagesMap.css'
+import { FiArrowRight, FiPlus } from 'react-icons/fi'
 
+import mapLocalMarker from '../images/local.svg'
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
+
+import '../styles/pages/orphanages-map.css'
+
+interface Orphanage {
+    id: number,
+    latitude: number,
+    longitude: number,
+    name: string
+}
 
 const OrphanagesMap = () => {
+
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+    useEffect(() => {
+        api.get('orphanages').then(res => setOrphanages(res.data))
+
+        // return () => {
+        //     cleanup
+        // };
+    }, []);
+
     return (
         <div id="page-map">
             <aside>
@@ -21,7 +40,7 @@ const OrphanagesMap = () => {
                 </header>
 
                 <footer>
-                    <strong>Juazeiro do Nrote</strong>
+                    <strong>Juazeiro do Norte</strong>
                     <span>Ceará</span>
                 </footer>
 
@@ -31,14 +50,37 @@ const OrphanagesMap = () => {
 
             <Map
                 center={[-7.2823667, -39.310245]}
-                zoom={15}
+                zoom={13}
                 style={{ width: '100%', height: '100%' }}
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                {orphanages.map(orphanage => (
+                    <Marker key={orphanage.id}
+                        icon={mapIcon}
+                        position={[orphanage.latitude, orphanage.longitude]}
+                    >
+                        <Popup
+                            closeButton={false}
+                            minWidth={240}
+                            maxWidth={240}
+                            className="map-popup"
+                        >
+                            {orphanage.name}
+                            <Link to={`/orphanages/${orphanage.id}`}>
+                                <FiArrowRight size={28} color="#FFF" />
+                            </Link>
+                        </Popup>
+                    </Marker>
+                ))}
+
             </Map>
 
-            <Link to="" className="create-orphanage">
+
+
+
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
